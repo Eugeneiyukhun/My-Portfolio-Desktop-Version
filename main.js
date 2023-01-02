6c06570dccdd1f40cc9245313799e229d4d60c55
Tao (Larissa) <25f5k252l@mozmail.com>
2023-01-03 00:03:35 +0300

    Add the function to open the menu

--- main.js
+++ main.js
@@ -1,4 +1,15 @@
 const mobileMenu = document.querySelector('.mobile-menu');
 const closeBtn = document.querySelector('.close-btn');
 const menuOption = document.querySelector('.menu-option');
-const body = document.querySelector('body')
+const body = document.querySelector('body');
+const headline = document.querySelector('.headline.generic-container')
+const openBtn = document.querySelector('.header-menu-button');
+
+const openMenu = () => {
+  body.classList.add('no-scroll');
+  headline.classList.add('blur');
+  mobileMenu.style.display = 'block';
+}
+
+
+openBtn.addEventListener('click', openMenu);
