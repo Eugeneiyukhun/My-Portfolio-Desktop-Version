f0f6a510e1c9dfc03b242805a40f8a2847ecde89
Eugeneiyukhun <eugenelegend@gmail.com>
2023-01-03 09:08:54 -0800

    Modified index.html and css/style.css

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const mobileMenu = document.querySelector('.mobile-menu');
 const closeBtn = document.querySelector('.close-btn');
-const menuOption = document.querySelector('.menu-option');
+const menuOption = document.querySelector('.menu-option li a');
 const body = document.querySelector('body');
 const headline = document.querySelector('.headline.generic-container')
 const openBtn = document.querySelector('.header-menu-button');
@@ -9,7 +9,15 @@ const openMenu = () => {
   body.classList.add('no-scroll');
   headline.classList.add('blur');
   mobileMenu.style.display = 'block';
+  popMenu.style.animation = 'fadein 2s';
 }
 
+const closeMenu = () => {
+    mobileMenu.style.display = 'none'; 
+    body.classList.remove('no-scroll');
+    headline.classList.remove('blur');
+  }
 
 openBtn.addEventListener('click', openMenu);
+closeBtn.addEventListener('click', closeMenu);
+menuOption.addEventListener('click', closeMenu);
