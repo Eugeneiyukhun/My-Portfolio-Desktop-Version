3c2ddfb2b6c741b450e1ca9ac12517d71aac9e3e
Eugeneiyukhun <eugenelegend@gmail.com>
2023-01-02 21:31:43 -0800

    Added main.js file

---
+++ main.js
@@ -0,0 +1,4 @@
+const mobileMenu = document.querySelector('.mobile-menu');
+const closeBtn = document.querySelector('.close-btn');
+const menuOption = document.querySelector('.menu-option');
+const body = document.querySelector('body')
