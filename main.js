5644e22345b20986dae5dda64241d747851a75e0
Eugeneiyukhun <eugenelegend@gmail.com>
2023-01-03 10:10:41 -0800

    Modified css/style.css and main.js script files

--- main.js
+++ main.js
@@ -2,21 +2,21 @@ const mobileMenu = document.querySelector('.mobile-menu');
 const closeBtn = document.querySelector('.close-btn');
 const menuOption = document.querySelector('.menu-option li a');
 const body = document.querySelector('body');
-const headline = document.querySelector('.headline.generic-container')
+const headline = document.querySelector('.headline.generic-container');
 const openBtn = document.querySelector('.header-menu-button');
 
 const openMenu = () => {
   body.classList.add('no-scroll');
   headline.classList.add('blur');
   mobileMenu.style.display = 'block';
-  popMenu.style.animation = 'fadein 2s';
-}
+  mobileMenu.style.animation = 'fadein 2s';
+};
 
 const closeMenu = () => {
-    mobileMenu.style.display = 'none'; 
-    body.classList.remove('no-scroll');
-    headline.classList.remove('blur');
-  }
+  mobileMenu.style.display = 'none';
+  body.classList.remove('no-scroll');
+  headline.classList.remove('blur');
+};
 
 openBtn.addEventListener('click', openMenu);
 closeBtn.addEventListener('click', closeMenu);
